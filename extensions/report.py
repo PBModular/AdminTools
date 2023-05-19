0de1c8b467e383e58b7760589a6b4d4a3692c57c
Ultra119 <pasasvistak0@gmail.com>
2023-05-19 15:35:34 +0300

    report: General code improving and refactoring
    - Simplified the if conditions by using setdefault method to create an empty dictionary if it doesn't exist.
    - Extracted the reply_user_id to improve code readability and reduce repetition.

--- extensions/report.py
+++ extensions/report.py
@@ -3,7 +3,6 @@ from base.module import command
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums import ChatMembersFilter
-
 import time
 
 class ReportExtension(ModuleExtension):
@@ -21,8 +20,9 @@ class ReportExtension(ModuleExtension):
         
         chat_id = message.chat.id
         user_id = message.from_user.id
-        
-        if user_id == message.reply_to_message.from_user.id:
+        reply_user_id = message.reply_to_message.from_user.id
+
+        if user_id == reply_user_id:
             await message.reply(self.S["report"]["yourself"])
             return
         
@@ -38,12 +38,9 @@ class ReportExtension(ModuleExtension):
                 cooldown_time = self.report_cooldown - int(current_time - last_report_time)
                 await message.reply(self.S["report"]["cooldown"].format(cooldown_time=cooldown_time))
                 return
-        
-        if chat_id not in self.last_report_times:
-            self.last_report_times[chat_id] = {}
-        
-        self.last_report_times[chat_id][user_id] = time.time()
-        
+
+        self.last_report_times.setdefault(chat_id, {})[user_id] = time.time()
+
         chat_title = message.chat.title or message.chat.username or message.chat.first_name
         user = message.from_user.mention
         reported_msg = message.reply_to_message_id
@@ -52,16 +49,22 @@ class ReportExtension(ModuleExtension):
         
         button_url = f"https://t.me/c/{str(chat_id)[4:]}/{reported_msg}"
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(self.S["report"]["msg"], url=button_url)]])
-        
+            [InlineKeyboardButton(self.S["report"]["msg"], url=button_url)]
+        ])
+
         async for admin in client.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS):
             if admin.user.is_bot:
                 continue
-            
-            await client.send_message(admin.user.id, self.S["report"]["alert"].format(
-                chat_title=chat_title, 
-                user=user, 
-                reported_user=reported_user, 
-                reported_text=reported_text), reply_markup=keyboard)
-            
+
+            await client.send_message(
+                admin.user.id,
+                self.S["report"]["alert"].format(
+                    chat_title=chat_title,
+                    user=user,
+                    reported_user=reported_user,
+                    reported_text=reported_text
+                ),
+                reply_markup=keyboard
+            )
+
         await message.reply(self.S["report"]["sent"])
