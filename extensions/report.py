33e6ecd456b6e7381bf2f7778d13c4fabd8a41a6
Ultra119 <pasasvistak0@gmail.com>
2023-05-21 21:06:38 +0300

    report: Transfer checks into separate func

--- extensions/report.py
+++ extensions/report.py
@@ -15,45 +15,17 @@ class ReportExtension(ModuleExtension):
     
     @command("report")
     async def report_cmd(self, bot: Client, message: Message):
-        if message.reply_to_message is None:
-            await message.reply(self.S["report"]["not_reply"])
+        if not await self.checks(bot, message):
+            # self.logger.warning(f"Report check failed for {message.from_user.id}")
             return
         
         chat_id = message.chat.id
-        user_id = message.from_user.id
-        reply_user_id = message.reply_to_message.from_user.id
-        is_bot = message.reply_to_message.from_user.is_bot
-        reason = " ".join(message.command[1:])
-        
-        reply_member = await bot.get_chat_member(chat_id=chat_id, user_id=reply_user_id)
-        if reply_member.status == ChatMemberStatus.ADMINISTRATOR and not is_bot or reply_member.status == ChatMemberStatus.OWNER:
-            await message.reply(self.S["report"]["admin"])
-            return
-
-        if user_id == reply_user_id:
-            await message.reply(self.S["report"]["yourself"])
-            return
-        
-        if is_bot:
-            await message.reply(self.S["report"]["bot"])
-            return
-        
-        if chat_id in self.last_report_times:
-            last_report_time = self.last_report_times[chat_id].get(user_id, 0)
-            current_time = time.time()
-            
-            if current_time - last_report_time < self.report_cooldown:
-                cooldown_time = self.report_cooldown - int(current_time - last_report_time)
-                await message.reply(self.S["report"]["cooldown"].format(cooldown_time=cooldown_time))
-                return
-
-        self.last_report_times.setdefault(chat_id, {})[user_id] = time.time()
-
         chat_title = message.chat.title or message.chat.username or message.chat.first_name
         user = message.from_user.mention
         reported_msg = message.reply_to_message_id
         reported_user = message.reply_to_message.from_user.mention
         reported_text = message.reply_to_message.text
+        reason = " ".join(message.command[1:])
         
         button_url = f"https://t.me/c/{str(chat_id)[4:]}/{reported_msg}"
         keyboard = InlineKeyboardMarkup([
@@ -69,9 +41,10 @@ class ReportExtension(ModuleExtension):
                     chat_title=chat_title,
                     user=user,
                     reported_user=reported_user,
-                    reported_text=reported_text
                 )
-
+                if reported_text:
+                    alert_text += "\n" + self.S["report"]["message"].format(reported_text=reported_text)
+                    
                 if reason:
                     alert_text += "\n" + self.S["report"]["reason"].format(reason=reason)
 
@@ -86,3 +59,39 @@ class ReportExtension(ModuleExtension):
                 continue
         
         await message.reply(self.S["report"]["sent"])
+        
+    async def checks(self, bot: Client, message: Message):
+        if message.reply_to_message is None:
+            await message.reply(self.S["report"]["not_reply"])
+            return False
+        
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        reply_user_id = message.reply_to_message.from_user.id
+        is_bot = message.reply_to_message.from_user.is_bot
+        
+        reply_member = await bot.get_chat_member(chat_id=chat_id, user_id=reply_user_id)
+        if reply_member.status == ChatMemberStatus.ADMINISTRATOR and not is_bot or reply_member.status == ChatMemberStatus.OWNER:
+            await message.reply(self.S["report"]["admin"])
+            return False
+
+        if user_id == reply_user_id:
+            await message.reply(self.S["report"]["yourself"])
+            return False
+        
+        if is_bot:
+            await message.reply(self.S["report"]["bot"])
+            return False
+    
+        if chat_id in self.last_report_times:
+            last_report_time = self.last_report_times[chat_id].get(user_id, 0)
+            current_time = time.time()
+            
+            if current_time - last_report_time < self.report_cooldown:
+                cooldown_time = self.report_cooldown - int(current_time - last_report_time)
+                await message.reply(self.S["report"]["cooldown"].format(cooldown_time=cooldown_time))
+                return False
+
+        self.last_report_times.setdefault(chat_id, {})[user_id] = time.time()
+
+        return True
